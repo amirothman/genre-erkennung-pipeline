91f8eee2b2585fcb82aed8c7945837f37993da09
Amir Othman <th3go2@gmail.com>
2016-05-15 23:04:05 +0200

    refactored

--- split_30_seconds_mono.py
+++ split_30_seconds_mono.py
@@ -9,15 +9,13 @@ def iterate_audio(format_ending="",path="."):
                 song_path = (os.path.join(root,name))
                 yield song_path
 
-def ffmpeg_process(filepath,format_ending,cmd):
+def ffmpeg_process(filepath,format_ending,cmd,delete_original=True):
     filepath = filepath.replace("./","")
     filepath = filepath.replace(".{0}".format(format_ending),"")
-    commandos = []
-    split_commando = cmd.format(filepath,format_ending)
-    commandos.append(split_commando)
-    del_commando = "rm {0}".format("".join([filepath,".",format_ending]))
-    commandos.append(del_commando)
-
+    commandos = [cmd.format(filepath,format_ending)]
+    if delete_original:
+        del_commando = "rm {0}".format("".join([filepath,".",format_ending]))
+        commandos.append(del_commando)
     for cmd in commandos:
         print(cmd)
         subprocess.call(cmd.split())
@@ -27,32 +25,14 @@ def thirty_seconds(filepath,format_ending):
     split audio to 30 seconds each.
 
     ffmpeg -i in.mp3 -f segment -segment_time 30 -c copy out%03d.mp3"""
-    filepath = filepath.replace("./","")
-    filepath = filepath.replace(".{0}".format(format_ending),"")
-    commandos = []
-    split_commando = "ffmpeg -i {0}.{1} -f segment -segment_time 30 -c copy {0}-%03d.{1}".format(filepath,format_ending)
-    commandos.append(split_commando)
-    del_commando = "rm {0}".format("".join([filepath,".",format_ending]))
-    commandos.append(del_commando)
-
-    for cmd in commandos:
-        print(cmd)
-        subprocess.call(cmd.split())
+    split_commando = "ffmpeg -i {0}.{1} -f segment -segment_time 30 -c copy {0}-%03d.{1}"
+    ffmpeg_process(filepath,format_ending,split_commando)
 
 def to_mono(filepath,format_ending):
     "to mono"
     "ffmpeg -i stereo.flac -ac 1 mono.flac"
-
-    filepath = filepath.replace("./","")
-    filepath = filepath.replace(".{0}".format(format_ending),"")
-    commandos = []
-    mono_commando = "ffmpeg -i {0}.{1} -ac 1 {0}-mono.{1}".format(filepath,format_ending)
-    commandos.append(mono_commando)
-    del_commando = "rm {0}".format("".join([filepath,".",format_ending]))
-    commandos.append(del_commando)
-    for cmd in commandos:
-        print(cmd)
-        subprocess.call(cmd.split())
+    mono_commando = "ffmpeg -i {0}.{1} -ac 1 {0}-mono.{1}"
+    ffmpeg_process(filepath,format_ending,mono_commando)
 
 def batch_thirty_seconds(file_format,folder_path):
     print("batch thirty_seconds")
@@ -69,7 +49,7 @@ def batch_mono(file_format,folder_path):
 if __name__=="__main__":
     # ffmpeg_process("02-Dreaming.mp3")
     file_format = "mp3"
-    folder_path = "dataset"
+    folder_path = "ngetest"
     print("proses")
     batch_thirty_seconds(file_format,folder_path)
     batch_mono(file_format,folder_path)
