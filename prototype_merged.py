17f18ef4f4d6aa718faae283ae288bb067dd8d5e
Amir Othman <th3go2@gmail.com>
2016-05-25 23:22:27 +0200

    revert to normal

--- prototype_merged.py
+++ prototype_merged.py
@@ -17,20 +17,20 @@ nb_epoch = 100
 
 print("creating model")
 # create model
-y = pickle.load(open("pickled_vectors/10_sec_split_gztan_mfcc_coefficients_label.pickle","rb"))
-y_test = pickle.load(open("pickled_vectors/10_sec_split_gztan_mfcc_coefficients_evaluation_label.pickle","rb"))
+y = pickle.load(open("pickled_vectors/gztanmfcc_coefficients_label.pickle","rb"))
+y_test = pickle.load(open("pickled_vectors/gztanmfcc_coefficients_evaluation_label.pickle","rb"))
 # X_1 = pickle.load(open("pickled_vectors/tempotracker_tempo_training_vector.pickle","rb"))
 # X_test_1 = pickle.load(open("pickled_vectors/tempotracker_tempo_evaluation_training_vector.pickle","rb"))
 
 # model_1 = tempotrack_model.tempotrack_model((X_1.shape[1],X_1.shape[2]))
 
-X_2 = pickle.load(open("pickled_vectors/10_sec_split_gztan_mfcc_coefficients_training_vector.pickle","rb"))
-X_test_2 = pickle.load(open("pickled_vectors/10_sec_split_gztan_mfcc_coefficients_evaluation_training_vector.pickle","rb"))
+X_2 = pickle.load(open("pickled_vectors/gztanmfcc_coefficients_training_vector.pickle","rb"))
+X_test_2 = pickle.load(open("pickled_vectors/gztanmfcc_coefficients_evaluation_training_vector.pickle","rb"))
 
 model_2 = mfcc_model.mfcc_model((X_2.shape[1],X_2.shape[2]))
 
-X_3 = pickle.load(open("pickled_vectors/10_sec_split_gztan_spectral-contrast_peaks_training_vector.pickle","rb"))
-X_test_3 = pickle.load(open("pickled_vectors/10_sec_split_gztan_spectral-contrast_peaks_evaluation_training_vector.pickle","rb"))
+X_3 = pickle.load(open("pickled_vectors/gztanspectral-contrast_peaks_training_vector.pickle","rb"))
+X_test_3 = pickle.load(open("pickled_vectors/gztanspectral-contrast_peaks_evaluation_training_vector.pickle","rb"))
 
 model_3 = spectral_contrast_peaks_model.model((X_3.shape[1],X_3.shape[2]))
 
@@ -61,9 +61,9 @@ final_model.compile(loss='categorical_crossentropy',
 
 
 
-json_string = final_model.to_json()
-with open("model_architecture/embeddings_10_sec_split_gztan_merged_model_architecture.json","w") as f:
-    f.write(json.dumps(json_string, sort_keys=True,indent=4, separators=(',', ': ')))
+# json_string = final_model.to_json()
+# with open("model_architecture/embeddings_10_sec_splitgztan_merged_model_architecture.json","w") as f:
+#     f.write(json.dumps(json_string, sort_keys=True,indent=4, separators=(',', ': ')))
 
 print("Fitting")
 # #
@@ -80,7 +80,7 @@ history = final_model.fit([X_2[:vector_length],X_3[:vector_length]], y[:vector_l
                             shuffle="batch"
                             )
 
-final_model.save_weights("model_weights/embeddings_10_sec_split_gztan_merged_model_weights.hdf5",overwrite=True)
+# final_model.save_weights("model_weights/embeddings_10_secsplit_gztan_merged_model_weights.hdf5",overwrite=True)
 
 with open("experimental_results.json","w") as f:
     f.write(json.dumps(history.history, sort_keys=True,indent=4, separators=(',', ': ')))
